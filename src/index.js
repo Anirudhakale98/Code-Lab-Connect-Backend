7be35a58ec1adaacdcb0fcc11499332003801d55
Anirudhakale98 <anirudhakale982354@gmail.com>
2024-08-05 18:36:25 +0530

    utils setup is done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js"
+import { app } from "./app.js";
 
 dotenv.config({ path: '../.env' });
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: app.on - ", error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8080, () =>{
+        console.log(`Sever is running on ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!", err);
+});
