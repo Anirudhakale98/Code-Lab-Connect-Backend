0476ed29ad03002fe0ace7eb1fdf4cef4697bc73
Anirudhakale98 <anirudhakale982354@gmail.com>
2024-08-06 19:44:52 +0530

    user controller and route is added

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
+//routes import 
+
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter); //http://localhost:5000/users/register
+
+
 export { app }; 
